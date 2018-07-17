76c376c3a4638aaad7c3b4760b105d0490097436
Ella Sklar <ella.soccer22@gmail.com>
2018-07-17 13:46:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,11 +5,11 @@ class App < Sinatra::Base
   get '/' do 
     "It was so awesome to see Karlie yesterday!!!She put a lot of our pictures and videos on her insta. She is so nice. I would love to meet her again!"
     
-    get '/kitty 'do 
+    get '/kitty' do 
       "Hello Kitty"
     end 
     
-    get '/lolipop'do 
+    get '/lolipop' do 
       "I like ice cream"
   end
 
